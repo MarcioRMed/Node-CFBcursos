5117252e58f66ddf09c16937fbb1bf178c7648a5
MarcioRMed <contato.microinfo@gmail.com>
2022-03-18 03:41:19 -0300

    aula4 - ok

---
+++ aula4b.js
@@ -0,0 +1,17 @@
+// instalação Express
+// npm install express --save
+
+
+const express = require('express');
+const app=express()
+const porta=process.env.PORT
+
+app.get('/',(req,res)=>{
+  res.send('Seja bem vindo')
+})
+
+app.get('/canal',(req,res)=> {
+  res.json({canal:'CFB Cursos'})
+})
+
+app.listen(porta || 3000,()=>{console.log('Servidor rodando na porta 3000')})
