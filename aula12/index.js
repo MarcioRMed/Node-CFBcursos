7dcdcf5f1c6ef0145d842500fe2704a3674f5b78
MarcioRMed <contato.microinfo@gmail.com>
2022-03-20 01:19:28 -0300

    aula 12 - ok

--- aula12/index.js
+++ aula12/index.js
@@ -1,35 +1,38 @@
 // Upload de arquivos em Node- módulo formidable
 
+//nao funcionou ???
+// link da aula
+// https://www.youtube.com/watch?v=ddgGeQvr67c&list=PLx4x_zx8csUjFC41ev2qX5dnr-0ThpoXE&index=12
+
 const http = require('http')
 const porta = process.env.PORT || 3000
 const formidavel = require('formidable')
 const fs = require('fs')
-const msg = 'servidor rodando porta 3000'
 
+// const msg = 'servidor rodando porta 3000'
 
 
 const servidor=http.createServer((req,res)=>{
-  if(req.url == '/envioDeArquivo'){
+  if (req.url == '/envioDeArquivo') {
     const form = new formidavel.IncomingForm()
-    form.parse(req,(erro, campos, arquivos)=>{
+    form.parse(req, (erro, campos, arquivos)=>{
       const urlantiga = arquivos.filetoupload.path
-      const urlnova = 'C:/Users/Márcio/Desktop/DADOS/DEV_Programação/CURSOS/PROFESSORES/NODE_JS/CFB/Node-CFBcursos/aula12/' + arquivos.filetoupload.name
+      const urlnova = 'C:/Users/Márcio/' + arquivos.filetoupload.name
+     
       fs.rename(urlantiga, urlnova, (erro)=>{
-        if(erro) throw erro
+        if (erro) throw erro
         res.write('Arquivo movido')
         res.end()
       })
     })
+
   }else{
-      res.writeHead(200,{'Content-Type':'text/html'})
+      res.writeHead(200, {'Content-Type': 'text/html'})
       res.write('<form action="envioDeArquivo" method="post" enctype="multipart/form-data">')
       res.write('<input type="file" name="filetoupload"><br>')
       res.write('<input type="submit" value="Enviar"')
       res.write('</form>')
       return res.end()
   }
-
-
-
 })
-servidor.listen(porta, console.log(msg))
+servidor.listen(porta)
