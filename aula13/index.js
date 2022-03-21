1189f81ac22d2e73f1c3db8df1ff2e62d9fe5083
MarcioRMed <contato.microinfo@gmail.com>
2022-03-20 22:05:14 -0300

    ajust aulas mongodb

--- aula13/index.js
+++ aula13/index.js
@@ -1,39 +1,122 @@
 // Usando mongoDB
 
-// aulas 13,14
+// aulas 13,14,15,16,17.18,19,20,21
 // npm install mongodb
-// 6VCEA6rGtXjxbncO   senha
 
 const mongodb=require('mongodb').MongoClient
 const url="mongodb+srv://marcio:6VCEA6rGtXjxbncO@cluster0.o0h8k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 mongodb.connect(url,(erro,banco)=>{                
   if(erro) throw erro
-
-
   const dbo=banco.db('cfbcursos')
-  const obj={curso:'Curso de Node',canal:'CBF Cursos'}
   const colecao='cursos'
+
+
+
+  // adicionar cursos===================================
+//   const obj=[
+//     {curso:'Curso de Node',canal:'CBF Cursos'},
+//     {curso:'Curso de Javascript',canal:'CBF Cursos'},
+//     {curso:'Curso de Arduino',canal:'CBF Cursos'},
+//     {curso:'Curso de C++',canal:'CBF Cursos'},
+//     {curso:'Curso de Java',canal:'CBF Cursos'}
+// ] 
+    
   
+// dbo.collection(colecao).insertMany(obj, async(erro,resultado)=>{
+//     if(erro) throw erro
+//     await console.log(resultado.insertedCount + ' novo(s) curso(s) inseridos')
+//     // banco.close()
+//   })
+
+
+
   
-  // inserir dados==========
+  // inserir dados=================================================================
   // dbo.collection(colecao).insertOne(obj,(erro,resultado)=>{
   //   if(erro) throw erro
   //   console.log('novo curso inserido')
   //   banco.close()
   // })
 
-  dbo.collection(colecao).find({}).toArray((erro,resultado)=>{ 
-  if(erro)throw erro
-  console.log(resultado)
-  banco.close()
-  })
+  // encontrar registro dentro da coleção====================================
+  // const query={curso: /C./} //query - consulta
+  // const query={} //todos
+
+  // dbo.collection(colecao).find(query,{projection:{_id:0}}).toArray((erro,resultado)=>{ 
+  //   if(erro)throw erro
+  //   // console.log(resultado)
+  //   console.log(resultado[2].curso)
+  //   banco.close()
+  //   })
+
+
+  // ordenação - pesquisa - método SORT==========================
+  
+  // const ordenação={curso:-1} //     1 crescente -1 decrecente
+  // const query={}
+
+  // dbo.collection(colecao).find(query).sort(ordenação).toArray((erro,resultado)=>{ 
+  //   if(erro)throw erro
+  //   console.log(resultado)
+  //   // console.log(resultado[2].curso)
+  //   banco.close()
+  //   })
+
+
+
+  // Deletar objeto da coleção====================
+
+  // let query={curso: 'Curso de Node'}
+  
+  // dbo.collection(colecao).deleteOne(query,(erro,resultado)=>{  
+  //   if(erro)throw erro
+  //   console.log('curso deletado')
+  //  })
+
+
+
+  // deleteMany- delete varios ao mesmo tempo===================
+  // let query ={curso:/.o/}   
+  // dbo.collection(colecao).deleteMany(query,async (erro,resultado)=>{ 
+  //   if(erro)throw erro
+  //   await console.log(resultado.deletedCount + ' curso(s) deletado(s)')
+  //  })
+
+  query={} //     listar após o delete
+  dbo.collection(colecao).find(query,{projection:{_id:0}}).toArray((erro,resultado)=>{ 
+    if(erro)throw erro
+    console.log(resultado)
+    banco.close()
+    })
+
+
 
 
 })
 
 
-// find mostra tudo
+// returna todo o contéudo
+// const query={} //todos
+// dbo.collection(colecao).find(query).toArray((erro,resultado)=>{ 
+
+
+
+
+// regex - query - consulta
+// /.t/ - terminando com a letra t
+// /C./ - lista todos com a letra c
+// const query={curso: /C./} //query - consulta
+// const query={} //todos
+
+
+
+
+
+
+// projection - _id:0 - nao mostra   id:1 - mostra
+
+// find mostra tudo - como se fosse um seleect * fron
 // dbo.collection(colecao).find({}).toArray((erro,resultado)=>{
 
 
