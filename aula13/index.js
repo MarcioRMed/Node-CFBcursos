fa1286c281c2091aeaefb11008628bc953536612
MarcioRMed <contato.microinfo@gmail.com>
2022-03-22 01:06:24 -0300

    aula 21 - 0k

--- aula13/index.js
+++ aula13/index.js
@@ -9,25 +9,74 @@ const url="mongodb+srv://marcio:6VCEA6rGtXjxbncO@cluster0.o0h8k.mongodb.net/myFi
 mongodb.connect(url,(erro,banco)=>{                
   if(erro) throw erro
   const dbo=banco.db('cfbcursos')
-  const colecao='cursos'
+  const colecao1='cursos'
+  const colecao2='detalhesCursos'
 
   // adicionar cursos===================================
-//   const obj=[
-//     {curso:'Curso de Node',canal:'CFB Cursos'},
-//     {curso:'Curso de Javascript',canal:'CFB Cursos'},
-//     {curso:'Curso de Arduino',canal:'CFB Cursos'},
-//     {curso:'Curso de C++',canal:'CFB Cursos'},
-//     {curso:'Curso de Java',canal:'CFB Cursos'}
-// ] 
+
+/*  
   
+  let obj=[
+    {idcurso:01,curso:'Curso de C++',canal:'CFB Cursos'},
+    {idcurso:02,curso:'Curso de HTML',canal:'CFB Cursos'},
+    {idcurso:03,curso:'Curso de CSS',canal:'CFB Cursos'},
+    {idcurso:04,curso:'Curso de Javascript',canal:'CFB Cursos'},
+    {idcurso:05,curso:'Curso de PHP',canal:'CFB Cursos'},
+    {idcurso:06,curso:'Curso de Node',canal:'CFB Cursos'},
+    {idcurso:07,curso:'Curso de React',canal:'CFB Cursos'},
+    {idcurso:08,curso:'Curso de React-Native',canal:'CFB Cursos'},
+    {idcurso:09,curso:'Curso de Arduino',canal:'CFB Cursos'},
+    {idcurso:10,curso:'Curso de Photoshop',canal:'CFB Cursos'},    
+    {idcurso:11,curso:'Curso de AppInventor',canal:'CFB Cursos'},
+    {idcurso:12,curso:'Curso de Flash',canal:'CFB Cursos'},
+    {idcurso:13,curso:'Curso de Unity',canal:'CFB Cursos'},
+    {idcurso:14,curso:'Curso de C#',canal:'CFB Cursos'},
+    {idcurso:15,curso:'Curso de Java',canal:'CFB Cursos'},
+    {idcurso:16,curso:'Curso de QT Creator',canal:'CFB Cursos'},
+    {idcurso:17,curso:'Curso de SQL',canal:'CFB Cursos'},
+    {idcurso:18,curso:'Curso de Python',canal:'CFB Cursos'}
+] 
   
-// dbo.collection(colecao).insertMany(obj, async(erro,resultado)=>{
-//     if(erro) throw erro
-//     await console.log(resultado.insertedCount + ' novo(s) curso(s) inseridos')
-//     banco.close()
-//   })
+// inserir varios objetos
+  
+dbo.collection(colecao1).insertMany(obj, async(erro,resultado)=>{
+    if(erro) throw erro
+    await console.log(resultado.insertedCount + ' novo(s) curso(s) inseridos')
+    // banco.close()
+  })
 
   
+
+obj=[
+  {idcurso:01, aulas:192},
+  {idcurso:02, aulas:264},
+  {idcurso:03, aulas:77},
+  {idcurso:04, aulas:122},
+  {idcurso:05, aulas:50},
+  {idcurso:06, aulas:21},
+  {idcurso:07, aulas:45},
+  {idcurso:08, aulas:46},
+  {idcurso:09, aulas:75},
+  {idcurso:10, aulas:30},
+  {idcurso:11, aulas:48},
+  {idcurso:12, aulas:60},
+  {idcurso:13, aulas:44},
+  {idcurso:14, aulas:132},
+  {idcurso:15, aulas:51},
+  {idcurso:16, aulas:89},
+  {idcurso:17, aulas:42},
+  {idcurso:18, aulas:86}
+]
+
+dbo.collection(colecao2).insertMany(obj, async(erro,resultado)=>{
+  if(erro) throw erro
+  await console.log(resultado.insertedCount + ' novo(s) curso(s) inseridos')
+  // banco.close()
+})
+
+
+*/
+
   // inserir dados=================================================================
   // dbo.collection(colecao).insertOne(obj,(erro,resultado)=>{
   //   if(erro) throw erro
@@ -42,18 +91,18 @@ mongodb.connect(url,(erro,banco)=>{
 
   // dbo.collection(colecao).find(query,{projection:{_id:0}}).toArray((erro,resultado)=>{ 
   //   if(erro)throw erro
-  //   // console.log(resultado)
-  //   console.log(resultado[2].curso)
+  //   console.log(resultado)
+  //   // console.log(resultado[2].curso)
   //   banco.close()
   //   })
 
 
   // ordenação - pesquisa - método SORT==========================
   
-  // const ordenação={curso:-1} //     1 crescente -1 decrecente
+  // const ordenação={curso:1} //     1 crescente -1 decrecente
   // const query={}
 
-  // dbo.collection(colecao).find(query).sort(ordenação).toArray((erro,resultado)=>{ 
+  // dbo.collection(colecao).find(query,{projection:{_id:0}}).sort(ordenação).toArray((erro,resultado)=>{ 
   //   if(erro)throw erro
   //   console.log(resultado)
   //   // console.log(resultado[2].curso)
@@ -91,32 +140,95 @@ mongodb.connect(url,(erro,banco)=>{
 
 
 // modificando /atualizando objetos=================== 
-// updateOne 
+// updateOne // updateMany
 
-// updateMany
+// Atualização de objetos 1 objeto
 
-// Atualização de objetos
-
-let query={curso: 'Curso de Javascript 2022'}
-let novoObj={$set:{curso: 'Curso de Javascript 2023'}}
+// let query={curso: 'Curso de Javascript 2022'}
+// let novoObj={$set:{curso: 'Curso de Javascript 2023'}}
 
 // dbo.collection(colecao).updateOne(query,novoObj,(erro,resultado)=>{ 
 //   if(erro)throw erro
 //   console.log('curso atualizado')
 // })
 
-// atualiza varios objetod de uma só vez
-dbo.collection(colecao).updateMany(query,novoObj,async(erro,resultado)=>{ 
-  if(erro)throw erro
-  await console.log(resultado.modifiedCount + ' cursos atualizados')
-})
+// atualiza varios objetos de uma só vez
+// dbo.collection(colecao).updateMany(query,novoObj,async(erro,resultado)=>{ 
+//   if(erro)throw erro
+//   await console.log(resultado.modifiedCount + ' cursos atualizados')
+// })
 
   
+
+
 // lista o banco de dados=====================
-query={}
-dbo.collection(colecao).find(query,{projection:{_id:0}}).toArray((erro,resultado)=>{
+// query={}
+// dbo.collection(colecao).find(query,{projection:{_id:0}}).toArray((erro,resultado)=>{
+// if(erro) throw erro
+// console.log(resultado)
+// banco.close()
+// })
+
+
+
+
+
+// limintando o número de objetos retornados
+// const query={curso: /.t/}
+// const limite = 5
+// // const query={}  //todos
+
+// dbo.collection(colecao)
+//    .find(query,{projection:{_id:0}})
+//    .limit(limite)
+//    .toArray((erro,resultado)=>{ 
+//     if(erro)throw erro
+//     console.log(resultado)
+//   // console.log(resultado[2].curso)
+//     banco.close()
+//   })
+
+
+
+
+
+
+// Juntando coleções com JOIN=====================
+
+// inserindo as tabelas
+// query={}
+// dbo.collection(colecao1).find(query,{projection:{_id:0}}).toArray((erro,resultado)=>{
+// if(erro) throw erro
+// console.log(resultado)
+// // banco.close()
+// })
+
+
+// query={}
+// dbo.collection(colecao2).find(query,{projection:{_id:0}}).toArray((erro,resultado)=>{
+// if(erro) throw erro
+// console.log(resultado)
+// // banco.close()
+// })
+
+
+
+
+
+// listagem para unir 2 coleções - JOIN
+let query={}
+dbo.collection(colecao1).aggregate([
+  { $lookup:{
+      from:'detalhesCursos',
+      localField:'idcurso',
+      foreignField:'idcurso',
+      as:'Detalhes'
+    }
+  }
+
+]).toArray((erro,resultado)=>{
 if(erro) throw erro
-console.log(resultado)
+console.log(JSON.stringify(resultado))
 banco.close()
 })
 
